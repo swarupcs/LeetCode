143f5382e3d7e4cc87f1c6e2adb33fa9ba5a3afc
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-11-26 20:38:01 +0530

    Time: 6 ms (59.18%), Space: 44.3 MB (6.58%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,38 +1,24 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        // Stores the resultant 
-  // string
-  String res = "";
+        int openCount = 0;
+        int closeCount = 0;
+        String result = "";
+        int start = 0;
  
-  // Stores the count of
-  // opened parentheses
-  int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                openCount++;
+            } else if (c == ')') {
+                closeCount++;
+            }
  
-  // Traverse the string
-  for (int c = 0; 
-           c < s.length(); c++) 
-  {
-    // If opening parentheses is
-    // encountered and their
-    // count exceeds 0
-    if (s.charAt(c) == '(' && 
-        count++ > 0)
+            if (openCount == closeCount) {
+                result += s.substring(start+1, i);
+                start = i+1;
+            }
+        }
  
-      // Include the character
-      res += s.charAt(c);
- 
-    // If closing parentheses is
-    // encountered and their
-    // count is less than count
-    // of opening parentheses
-    if (s.charAt(c) == ')' && 
-        count-- > 1)
- 
-      // Include the character
-      res += s.charAt(c);
-  }
- 
-  // Return the resultant string
-  return res;
+        return result;
     }
 }
