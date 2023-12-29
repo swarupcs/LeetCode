412a7fc1118920c797bf9b66cf9e44d649527ec3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2023-12-29 22:06:59 +0530

    Time: 3 ms (18.12%), Space: 42.2 MB (16.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,41 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-         // Base condition
-        if (s == null || s.length() < 1) {
-            return 0;
-        }
-        // MAX and MIN values for integers
-        final int INT_MAX = 2147483647;
-        final int INT_MIN = -2147483648;
-        // Trimmed string
-        s = s.replaceAll("^\\s+", "");
-        // Counter
-        int i = 0;
-        // Flag to indicate if the number is negative
-        boolean isNegative = s.startsWith("-");
-        // Flag to indicate if the number is positive
-        boolean isPositive = s.startsWith("+");
-        if (isNegative) {
-            i++;
-        } else if (isPositive) {
+        int result=0;    // helper variables
+        int i=0;
+        int sign=1;
+    
+        while(i<s.length()&&s.charAt(i)==' ')i++;  //ignore leading white space
+        if(i==s.length())return 0;
+        if(s.charAt(i)=='-'||s.charAt(i)=='+')          //check if number positve or negative
+        {
+            sign=s.charAt(i)=='-'?-1:1;
             i++;
         }
-        // This will store the converted number
-        double number = 0;
-        // Loop for each numeric character in the string iff numeric characters are leading
-        // characters in the string
-        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            number = number * 10 + (s.charAt(i) - '0');
+        // now iterate across digits if any
+    // should only be in range 0-9
+        while(i<s.length()&&(s.charAt(i)>='0'&&s.charAt(i)<='9'))  //traverse string till nondigit not found or string ends
+        {
+            int digit=(s.charAt(i)-'0')*sign;
+            if(sign==1 && (result>Integer.MAX_VALUE/10 || (result==Integer.MAX_VALUE/10 && digit>Integer.MAX_VALUE%10))) return Integer.MAX_VALUE; //check for overflow
+            if(sign==-1 &&(result<Integer.MIN_VALUE/10 || (result==Integer.MIN_VALUE/10 && digit<Integer.MIN_VALUE%10))) return Integer.MIN_VALUE; //check for underflow
+            
+            result=result*10+digit; // update result
             i++;
         }
-        // Give back the sign to the converted number
-        number = isNegative ? -number : number;
-        if (number < INT_MIN) {
-            return INT_MIN;
-        }
-        if (number > INT_MAX) {
-            return INT_MAX;
-        }
-        return (int) number;
+    
+    return result;
     }
 }
