6fa80a8ea6aa3045aecb1e93f68c43f06f0f1405
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-22 22:28:56 +0530

    Time: 16 ms (17.53%), Space: 63.9 MB (11.51%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    let st = [];
+    for(let i = 0; i < num.length; i++) {
+        let digit = num[i];
+
+        while(st.length > 0 && k > 0 && st[st.length - 1] > digit) {
+            st.pop();
+            k--;
+        }
+
+        st.push(digit);
+    }
+
+    while(st.length > 0 && k > 0) {
+        st.pop();
+        k--;
+    }
+
+    // Handling edge case
+        if(st.length === 0) return "0";
+        
+        // To store the result
+        let res = "";
+        
+        // Adding digits in stack to result
+        while(st.length > 0) {
+            res += st.pop();
+        }
+        
+        // Trimming the zeroes at the back
+        res = res.replace(/0+$/, '');
+        
+        // Reverse to get the actual number
+        res = res.split('').reverse().join('');
+        
+        // Edge case
+        if(res.length === 0) return "0";
+        
+        // Return the stored result
+        return res;
+
+
+    
+};
