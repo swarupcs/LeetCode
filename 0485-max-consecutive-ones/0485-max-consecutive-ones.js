dd88c68c46faf7437a4eb76ba3ac5099d2bae467
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-01-14 22:00:30 +0530

    Time: 2 ms (71.85%), Space: 59.8 MB (55.42%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let maxCount = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 1) {
+            count++;
+            maxCount = count > maxCount ? count : maxCount;
+        } else {
+            count = 0;
+        }
+    }
+    
+    return maxCount;
+};
