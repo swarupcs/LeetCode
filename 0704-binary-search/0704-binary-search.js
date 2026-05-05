fdc100ae8647963e96704dfe11dfe31547529c69
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-05-05 22:38:40 +0530

    Time: 0 ms (100%), Space: 57 MB (93.63%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let n = nums.length;
+    let low = 0;
+    let high = n - 1;
+
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if(nums[mid] === target) {
+            return mid;
+        } else if(nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+};
