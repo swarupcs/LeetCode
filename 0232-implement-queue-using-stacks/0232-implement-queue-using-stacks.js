e04817d377dcbbe2a88abd9e0a9cff69d3bd4ad7
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-06-24 08:57:01 +0530

    Time: 0 ms (100%), Space: 53.3 MB (69.94%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -0,0 +1,48 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.input = [];
+    this.output = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.input.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.output.length === 0) {
+        while (this.input.length > 0) {
+            this.output.push(this.input.pop());
+        }
+    }
+
+    return this.output.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if (this.output.length === 0) {
+        while (this.input.length > 0) {
+            this.output.push(this.input.pop());
+        }
+    }
+
+    return this.output[this.output.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.input.length === 0 && this.output.length === 0;
+};
