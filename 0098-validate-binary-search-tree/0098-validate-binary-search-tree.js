c77c4821cf4171056b0386e07809d68f84036d4f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-07-11 11:01:46 +0530

    Time: 1 ms (59.07%), Space: 60.3 MB (8.72%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.left = (left === undefined ? null : left)
+ *     this.right = (right === undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    const validate = (node, min, max) => {
+        if (node === null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return (
+            validate(node.left, min, node.val) &&
+            validate(node.right, node.val, max)
+        );
+    };
+
+    return validate(root, -Infinity, Infinity);
+};
